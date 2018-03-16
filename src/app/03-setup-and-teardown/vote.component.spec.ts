86ef960a9a39078fd074f912989017087c5d69f9
EugeniaM <maltsevae88@gmail.com>
2018-03-17 00:31:45 +0200

    Init

---
+++ src/app/03-setup-and-teardown/vote.component.spec.ts
@@ -0,0 +1,6 @@
+import { VoteComponent } from './vote.component';
+
+describe('VoteComponent', () => {
+  it('', () => {
+  });
+});
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LikeComponent } from './ex2- like-component/like.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LikeComponent
   ],
   imports: [
     BrowserModule
